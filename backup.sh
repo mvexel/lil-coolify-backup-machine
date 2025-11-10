5f0d46f1277a5727ea4bce0a00e387d20c46023b
Martijn van Exel <m@rtijn.org>
2025-11-10 16:07:32 -0700

    oops dir

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ BUCKET="${S3_BUCKET}"
 REMOTE=":s3,rclonetype=s3,provider=Cloudflare,env_auth=false,access_key_id=${S3_ACCESS_KEY_ID},secret_access_key=${S3_SECRET_ACCESS_KEY},endpoint=${S3_ENDPOINT}"
 
 # stream volumes -> R2
-tar -czf - -C /data volumes \
+tar -czf - -C /data \
   | rclone rcat "${REMOTE}${BUCKET}/${BACKUP_NAME}" --s3-no-check-bucket
 
 # retention
